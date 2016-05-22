e0abae8dfef2fe672fb6c3fa80f86ef9f3c33d86
Kacper Rogowski <rogemus@gmail.com>
2016-05-23 00:30:14 +0200

    Uzytkownik nie jest wylogowywany po przeladowaniu strony

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import reduxThunk from 'redux-thunk';
 import {Provider} from 'react-redux';
 import {createStore, applyMiddleware} from 'redux';
 import {Router, Route, IndexRoute, browserHistory} from 'react-router';
-
+import {AUTH_USER} from './actions/types';
 
 import App from './components/app';
 import Signin from './components/auth/signin';
@@ -16,16 +16,24 @@ import RequireAuth from './components/auth/require_auth';
 import reducers from './reducers';
 
 const createStoreWithMiddleware = applyMiddleware(reduxThunk)(createStore);
+const store = createStoreWithMiddleware(reducers);
+
+const token = localStorage.getItem('token');
+
+if (token) {
+    store.dispatch({type: AUTH_USER});
+}
+
 
 ReactDOM.render(
-    <Provider store={createStoreWithMiddleware(reducers)}>
+    <Provider store={store}>
         <Router history={browserHistory}>
             <Route path="/" component={App}>
-                <IndexRoute component={Home} />
-                <Route path="signin" component={Signin} />
-                <Route path="signup" component={Signup} />
-                <Route path="signout" component={Signout} />
-                <Route path="feature" component={RequireAuth(Feature)} />
+                <IndexRoute component={Home}/>
+                <Route path="signin" component={Signin}/>
+                <Route path="signup" component={Signup}/>
+                <Route path="signout" component={Signout}/>
+                <Route path="feature" component={RequireAuth(Feature)}/>
             </Route>
         </Router>
     </Provider>
