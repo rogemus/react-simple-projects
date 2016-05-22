a90f09a29928d93913db6e7e41db9d8e1d8afa2d
Kacper Rogowski <rogemus@gmail.com>
2016-05-22 23:52:02 +0200

    Dodanie signup action

--- src/actions/index.js
+++ src/actions/index.js
@@ -23,15 +23,28 @@ export function signinUser({email, password}) {
     }
 }
 
-export function authError(error) {
-    return {
-        type: AUTH_ERROR,
-        payload: error
-    }
-}
 export function signoutUser() {
     localStorage.removeItem('token');
     return {
         type: UNAUTH_USER
     };
+}
+
+export function signupUser({ email, password }) {
+    return function(dispatch) {
+        axios.post(`${ROOT_URL}/signup`, { email, password })
+            .then(response => {
+                dispatch({ type: AUTH_USER });
+                localStorage.setItem('token', response.data.token);
+                browserHistory.push('/feature');
+            })
+            .catch(response => dispatch(authError(response.data.error)));
+    }
+}
+
+export function authError(error) {
+    return {
+        type: AUTH_ERROR,
+        payload: error
+    }
 }
