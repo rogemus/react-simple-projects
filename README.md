8b8895a0932e18ee56ca48f271ef3e677660e8e2
Kacper Rogowski <rogemus@gmail.com>
2016-05-22 23:41:06 +0200

    init

---
+++ README.md
@@ -0,0 +1,11 @@
+```
+	> npm install
+	> npm start
+```
+
+
+###Server
+```
+https://github.com/rogemus/Simple-NodeJs-Jwt
+```
+
---
+++ src/components/header.js
@@ -0,0 +1,43 @@
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
+import {Link} from 'react-router';
+
+class Header extends Component {
+
+    renderLinks() {
+        if (this.props.authenticated == true) {
+            return (
+                <li className="nav-item">
+                    <Link className="nav-link" to="/signout">Sign Out</Link>
+                </li>)
+        } else {
+            return [
+                <li className="nav-item" key={1}>
+                    <Link className="nav-link" to="/signin">Sign in</Link>
+                </li>,
+                <li className="nav-item" key={2}>
+                    <Link className="nav-link" to="/signup">Sign up</Link>
+                </li>
+
+            ]
+        }
+    }
+
+    render() {
+        return (
+            <nav className="navbar navbar-light">
+                <Link to="/" className="navbar-brand">Home</Link>
+                <ul className="nav navbar-nav">
+                    {this.renderLinks()}
+                </ul>
+            </nav>
+        )
+    }
+}
+function mapStateToPorps(state) {
+    return {
+        authenticated: state.auth.authenticated
+    }
+}
+
+export default connect(mapStateToPorps)(Header);
