3a934f35ce3b8fba41f7b76f0fb1d952de98eea7
Kacper Rogowski <rogemus@gmail.com>
2016-10-25 18:46:54 +0200

    update

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,6 @@
-import { combineReducers } from 'redux';
+import {combineReducers} from 'redux';
+import weatherReducer from './reducer_weather'
 
-const rootReducer = combineReducers({
-  state: (state = {}) => state
-});
+const rootReducer = combineReducers({weather: weatherReducer});
 
 export default rootReducer;
